2a0c46e9ab7c693804b92e5fb98e51d83f031cf3
atrimper <liv4waterville@gmail.com>
2020-11-05 17:45:37 -0500

    Changed Gym example to show QLearner is working

--- QLearning/GymExample.py
+++ QLearning/GymExample.py
@@ -5,10 +5,11 @@ import QLearningAgent
 #env = gym.make('Acrobot-v1')
 #env = gym.make('CartPole-v0')
 #env = gym.make('MountainCar-v0')
-env = gym.make('FrozenLake-v0')
-QLearner = QLearningAgent.QLearningAgent(env.observation_space, env.action_space, epsilon=0.2, alpha=0.2, discount=0.9)
+#env = gym.make('FrozenLake-v0')
+env = gym.make('Taxi-v3')
+QLearner = QLearningAgent.QLearningAgent(env.observation_space, env.action_space, epsilon=0.3, alpha=0.2, discount=0.9)
 
-num_episodes = 1000
+num_episodes = 10000
 for episode in range(0, num_episodes):
     observation = env.reset()
     for t in range(0, 100):
@@ -18,21 +19,25 @@ for episode in range(0, num_episodes):
         observation = next_observation
         if done:
             break
-
-    if episode % 100 == 0:
-        util.printProgressBar(episode, num_episodes, prefix='Training Completion')
+    
+    if episode % (num_episodes / 100) == 0:
+        util.printProgressBar(episode, num_episodes)
 
 print("DONE TRAINING")
 
-for episode in range(0, 1):
+for episode in range(0, 4):
     observation = env.reset()
+    env.render()
     for t in range(0, 100):
-        env.render()
         action = QLearner.getAction(observation, False)
         next_observation, reward, done, info = env.step(action)
-        #QLearner.update(observation.tobytes(), action, next_observation.tobytes(), reward)
+        env.render()
         observation = next_observation
         if done:
+            print("Success!")
+            print("")
+            print("-------------------------------------------")
+            print("")
             break
 
 env.close()
