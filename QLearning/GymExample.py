ec0f191ee540d83d79d8d3392a2c7fc8c77584f4
atrimper <liv4waterville@gmail.com>
2020-11-06 15:21:32 -0500

    Cleaned up GymExample and reduced training time

--- QLearning/GymExample.py
+++ QLearning/GymExample.py
@@ -2,17 +2,13 @@ import util
 import gym
 import QLearningAgent
 
-#env = gym.make('Acrobot-v1')
-#env = gym.make('CartPole-v0')
-#env = gym.make('MountainCar-v0')
-#env = gym.make('FrozenLake-v0')
 env = gym.make('Taxi-v3')
 QLearner = QLearningAgent.QLearningAgent(env.observation_space, env.action_space, epsilon=0.3, alpha=0.2, discount=0.9)
 
-num_episodes = 10000
+num_episodes = 1000
 for episode in range(0, num_episodes):
     observation = env.reset()
-    for t in range(0, 100):
+    while True:
         action = QLearner.getAction(observation)
         next_observation, reward, done, info = env.step(action)
         QLearner.update(observation, action, next_observation, reward)
@@ -28,7 +24,7 @@ print("DONE TRAINING")
 for episode in range(0, 4):
     observation = env.reset()
     env.render()
-    for t in range(0, 100):
+    for i in range(0, 1000):
         action = QLearner.getAction(observation, False)
         next_observation, reward, done, info = env.step(action)
         env.render()
