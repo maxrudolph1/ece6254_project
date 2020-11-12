0ca57b5abde04004e24ba068b6c4df82d61720e5
Max Rudolph <maxrudolph@Maxs-MacBook-Pro-3.local>
2020-11-12 18:08:34 -0500

    demo

--- dqn/train.py
+++ dqn/train.py
@@ -9,20 +9,27 @@ from TrafficEnvironment import traffic_environment as te
 import numpy as np
 import torch
 
-env = te.TrafficEnv(horiz_lanes=('e','e'), vert_lanes=('n','s','n'), 
-horiz_sizes=(3,3,3,3), vert_sizes=(3,3,3), car_speed=2, max_steps=1000)
+env = te.TrafficEnv(horiz_lanes=('e','e', 'e'), vert_lanes=('n','n','s'), 
+horiz_sizes=(3,3,3,3), vert_sizes=(3,3,3,3), car_speed=2, max_steps=1000)
 
 model = Net(env.observation_space.shape[0], env.action_space.shape[0])
 
 
+print(env.observation_space.shape)
+print(env.action_space)
 env.render()
 done = False
+count = 0
 while not done:
-    action = np.zeros((6, 1))
+    action = np.zeros((env.action_space.shape))
+    action[np.random.random((env.action_space.shape)) > 0] = 1
     observation, reward, done, _ = env.step(action)
-    obs = np.zeros(env.observation_space.shape)
-    obs[observation] = 1
-
-    print(model(torch.Tensor(obs)))
+    count += 1
+    if count > 2:
+        break
     env.render()
-    done = True
+
+
+
+env.render()
+    
